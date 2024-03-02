965c03e6a7c6d887b46a0da9880d5519b0d50b0c
AlexanderTolstov <161148767+AlexanderTolstov@users.noreply.github.com>
2024-03-02 20:18:18 +0700

    Add files via upload

---
+++ Работа со словарями.py
@@ -0,0 +1,13 @@
+my_list = ['Arsenal', 'Chelsea', 'Tottenham', 'Everton', 'Liverpool']
+print (my_list)
+print (my_list[0])
+print (my_list[-1])
+print (my_list[2:4])
+my_list[2] = 'Manchester United'
+print (my_list)
+
+my_dict = {'iron':'железо', 'steel':'сталь', 'silver':'серебро', 'gold':'золото'}
+print (my_dict)
+print (my_dict['iron'])
+my_dict['bronze'] = 'бронза'
+print(my_dict)
