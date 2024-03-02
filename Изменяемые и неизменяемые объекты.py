c5b7c9ffe26613194d78af70ef9281a1fe2fa413
AlexanderTolstov <161148767+AlexanderTolstov@users.noreply.github.com>
2024-03-02 19:41:09 +0700

    Add files via upload

---
+++ Изменяемые и неизменяемые объекты.py
@@ -0,0 +1,8 @@
+immutable_var = 1, 2, 3, 'a', 'b', 'c'
+print(immutable_var)
+
+mutable_list = ['Arsenal', 'Chelsea', 'Liverpool', 'Everton']
+print (mutable_list)
+mutable_list[1] = 'Barcelona'
+print(mutable_list)
+
