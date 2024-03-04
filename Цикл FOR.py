3ed9b81fe6a9a53483c329f795fd0920e5aaa4ec
AlexanderTolstov <161148767+AlexanderTolstov@users.noreply.github.com>
2024-03-04 12:52:19 +0700

    Add files via upload

---
+++ Цикл FOR.py
@@ -0,0 +1,7 @@
+list_ = ['FORD', 'AUDI', 'MAZDA', 'TOYOTA', 'LAMBORGHINI', 'BMW', 'HONDA']
+for i in list_:
+    print ('Я езжу на автомобиле марки ', i)
+cars_count = 0
+for j in list_:
+    cars_count += 10
+    print (cars_count)
