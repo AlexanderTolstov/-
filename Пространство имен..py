2e4941d3a74880708ce683164b2094fb942693d9
AlexanderTolstov <161148767+AlexanderTolstov@users.noreply.github.com>
2024-04-05 12:23:39 +0700

    Delete Пространство имен..py

--- Пространство имен..py
+++
@@ -1,9 +0,0 @@
-def test_function():
-    def inner_function():
-        print ('Я в области видимости функции test_function')
-    inner_function()
-
-test_function()
-
-inner_function() # Данная строчка кода работать не будет, так как эта функция находится в локальном пространстве имен,
-# и вызвать ее вне данного пространства нельзя.
