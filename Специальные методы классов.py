89055d2fc283b69e13792df577d8fcef3351046a
AlexanderTolstov <161148767+AlexanderTolstov@users.noreply.github.com>
2024-05-15 16:10:07 +0700

    Delete Специальные методы классов.py

--- Специальные методы классов.py
+++
@@ -1,9 +0,0 @@
-class House:
-    def __init__(self):
-        self.floors = 0
-    def setNewNumbersOfFloors(self, floors):
-        self.floors = floors
-myhouse = House()
-myhouse.setNewNumbersOfFloors(99)
-print(myhouse.floors)
-
