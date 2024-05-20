cbc0a74b44bbf7ea0d33b83fac200c80ce49546e
AlexanderTolstov <161148767+AlexanderTolstov@users.noreply.github.com>
2024-05-20 13:39:42 +0700

    Add files via upload

---
+++ Try и Except.py
@@ -0,0 +1,10 @@
+def add_everything_up(a, b):
+    return a + b
+try:
+    add_everything_up(123.456, 'строка')
+    add_everything_up('яблоко', 4215)
+    add_everything_up(123.456, 7)
+except TypeError:
+    print(add_everything_up(str(123.456), 'строка'))
+    print(add_everything_up('яблоко', str(4215)))
+    print(add_everything_up(123.456, 7))
