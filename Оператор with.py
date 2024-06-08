942bdf36e89ba7a0cf7f9f53a52676521f63d42d
AlexanderTolstov <161148767+AlexanderTolstov@users.noreply.github.com>
2024-06-08 15:53:39 +0700

    Delete Оператор with.py

--- Оператор with.py
+++
@@ -1,8 +0,0 @@
-file_name = 'Текстовый документ.txt.'
-with open(file_name, mode='r', encoding='utf8') as file:
-    file_content = file.read()
-    print(file_content)
-
-print(file.closed)
-# Использование оператора with open(file_name...) от простого использования file.close() отличается тем, что оператор
-# with закрывает файл автоматичски.
