79880e82c1261f5d98cacb26e3eeaf5d183c306e
AlexanderTolstov <161148767+AlexanderTolstov@users.noreply.github.com>
2024-07-27 13:14:50 +0700

    Add files via upload

---
+++ Создание функций на лету.py
@@ -0,0 +1,38 @@
+
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+res = list(map(lambda x, y: x == y, first, second))
+print(res)
+
+def get_advanced_writer(file_name):
+    def write_everything(*data_set):
+        with open(file_name, mode='w', encoding='utf-8') as file:
+            for line in data_set:
+                file.write(str(line))
+                file.write('\n')
+    return write_everything
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+from random import choice
+class MysticBall:
+    def __init__(self, *word):
+        self.word = word
+    def __call__(self):
+        word = choice(self.word)
+        return word
+
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print(first_ball())
+print(first_ball())
+print(first_ball())
+print(first_ball())
+print(first_ball())
+print(first_ball())
+print(first_ball())
+print(first_ball())
+print(first_ball())
+print(first_ball())
+
+
+
